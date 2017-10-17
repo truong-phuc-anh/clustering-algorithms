cfa59e3387aee3733e17088c51d4c66862f6b8aa
Truong Phuc Anh <truongphucanh.main@gmail.com>
2017-10-17 22:18:14 +0700

    feat: Add visualizing and evaluation calculating for exercise 1

--- src/1. Gaussian data/kmeans.py
+++ src/1. Gaussian data/kmeans.py
@@ -2,26 +2,40 @@
 # Simple implementation of Kmeans cluster
 
 import matplotlib.pyplot as plt
+import sklearn
 
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 
-# create input data (unlable)
-n_samples = 2500
+# 1. Create data set
+print "1. Creating data set..."
+n_samples = 2000
 random_state = 1500
 n_clusters = 2
-x, y = make_blobs(n_samples=n_samples, n_features=n_clusters, centers=n_clusters, random_state=random_state)
+data, labels_true = make_blobs(n_samples=n_samples, n_features=n_clusters, centers=n_clusters, random_state=random_state)
+print "Done"
 
-# lable using KMeans
+# 2. Cluster with kmeans
+print "2. Clustering..."
 cluster = KMeans(n_clusters=n_clusters, random_state=random_state)
-result = cluster.fit_predict(x)
+labels_pred = cluster.fit_predict(data)
+print "Done"
 
-# get centers of clusters
-centers = cluster.cluster_centers_
+# 3. Calculate evaluation using NMI (Normalized Mutual Infomation)
+print "3. Calculating evaluation using NMI (Normalized Mutual Infomation)..."
+NMI = sklearn.metrics.normalized_mutual_info_score(labels_pred, labels_true)
+print "NMI = ", NMI
+print "Done"
 
-# visualize
-plt.figure()
-plt.scatter(x[:, 0], x[:, 1], c=result)
+# 4. Visualize 
+print "4. Visualizing..."
+centers = cluster.cluster_centers_ # get centers of clusters
+plt.figure('labels predit')
+plt.scatter(data[:, 0], data[:, 1], c=labels_pred)
 plt.scatter(centers[:,0], centers[:, 1])
 
+plt.figure('labels true')
+plt.scatter(data[:, 0], data[:, 1], c=labels_true)
+
 plt.show()
+
