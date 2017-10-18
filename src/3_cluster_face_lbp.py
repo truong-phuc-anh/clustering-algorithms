47555f0fc5a3003e8d52850a13412cfbcf04c076
Truong Phuc Anh <truongphucanh.main@gmail.com>
2017-10-18 22:10:50 +0700

    doc: Change folder structure

--- src/3_cluster_face_lbp.py
+++ src/3_cluster_face_lbp.py
@@ -1,7 +1,7 @@
 
 # Created by Truong Phuc Anh (14520040@gm.uit.edu.vn)
-# 16/10/2017
-# Kmeans and Spetral cluster on faces data with HOG feature space
+# 06/10/2017
+# Kmeans and Spetral cluster on faces data
 
 import matplotlib.pyplot as plt
 import numpy
@@ -24,43 +24,45 @@ print('Loaded data set\n\n')
 
 print('Num images: ' + str(len(faces.images)) + '\n\n')
 
-# settings for hog
-hog_images = []
-hog_images_file_path = './././data/faces/hog_images.npz'
+# settings for LBP
+radius = 3
+n_points = 8 * radius
+lbp_images = []
+lbp_images_file_path = '././data/faces/lbp_images.npz'
 
-# if hog feature has been computed before, use it
-if os.path.isfile(hog_images_file_path):
-    # loading hog feature from saved file
-    print('Loading hog feature from ' + hog_images_file_path + '...')
-    npz_file = numpy.load(hog_images_file_path)
+# if lbp feature has been computed before, use it
+if os.path.isfile(lbp_images_file_path):
+    # loading lbp feature from saved file
+    print('Loading lbp feature from ' + lbp_images_file_path + '...')
+    npz_file = numpy.load(lbp_images_file_path)
     for file in npz_file.files:
-        hog_images.append(npz_file[file])
-    print( 'Loaded ' + str(len(hog_images)) + ' hog images\n\n' )
+        lbp_images.append(npz_file[file])
+    print( 'Loaded ' + str(len(lbp_images)) + ' lbp images\n\n' )
 
 # if it has not been computed.
 else:
-    # computes hog feature
-    print('Computting hog feature...')
+    # computes lbp feature
+    print('Computting lbp feature...')
     for image in faces.images:
-        fd, hog_image = skimage.feature.hog(image, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualise=True)
-        hog_images.append(hog_image.flatten()) # save as 1D Array
-    print('Computed hog feature\n\n')
-    print('Num hog images: ' + str(len(hog_images)) + '\n\n')
+        lbp = skimage.feature.local_binary_pattern(image, n_points, radius) # compute lbs feature, 2D Array
+        lbp_images.append(lbp.flatten()) # save as 1D Array
+    print('Computed lbp feature\n\n')
+    print('Num lbp images: ' + str(len(lbp_images)) + '\n\n')
     # saves it to file for the next time
-    print('Saving hog to ' + hog_images_file_path + '...')
-    numpy.savez(hog_images_file_path, *[hog_images[i] for i in range(len(hog_images))])
-    print('Saved hog to ' + hog_images_file_path + '\n\n')
+    print('Saving LBP to ' + lbp_images_file_path + '...')
+    numpy.savez(lbp_images_file_path, *[lbp_images[i] for i in range(len(lbp_images))])
+    print('Saved LBP to ' + lbp_images_file_path + '\n\n')
 
 # config for cluster
 n_persons = 60 # from http://vis-www.cs.umass.edu/lfw/#informatio
 # clusters 
 print('Kmeans Clustering...')
 cluster = sklearn.cluster.KMeans(n_clusters = n_persons, random_state = 1500)
-kmeans_labels = cluster.fit_predict(hog_images)
+kmeans_labels = cluster.fit_predict(lbp_images)
 print('Clustered\n\n')
 
 print('Spetral Clustering...')
-graph = cosine_similarity(hog_images)
+graph = cosine_similarity(lbp_images)
 spetral_labels = sklearn.cluster.spectral_clustering(graph, n_clusters = n_persons)
 print('Clustered\n\n')
 
