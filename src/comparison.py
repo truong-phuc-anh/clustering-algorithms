19e713276e1a0e9b3c9a7ec7a9aa9a0972c8f099
Truong Phuc Anh <truongphucanh.main@gmail.com>
2017-10-19 14:17:33 +0700

    feat: Add visualization task in comparison.py

--- src/comparison.py
+++ src/comparison.py
@@ -1,8 +1,10 @@
+import matplotlib.pyplot        as plt
 from time                       import time
 from sklearn                    import datasets
 from sklearn.cluster            import KMeans, spectral_clustering, DBSCAN, AgglomerativeClustering
 from sklearn.metrics.pairwise   import cosine_similarity
 from evaluation                 import evaluate_clustering, evaluate_with_predited_labels
+from sklearn.decomposition      import PCA
 
 def cluster_and_compare(n_clusters, data, labels_true):
     print(75 * '-')
@@ -17,8 +19,36 @@ def cluster_and_compare(n_clusters, data, labels_true):
     execution_time = time() - start_time
     evaluate_with_predited_labels(labels_true, spectral_labels, execution_time, "spectral")
 
-    dbscan_cluster = DBSCAN()
+    dbscan_cluster = DBSCAN(eps = 0.0595, min_samples = 10, metric='cosine')
     dbscan_labels = evaluate_clustering(dbscan_cluster, "DBSCAN", data, labels_true)
 
     agg_cluster = AgglomerativeClustering(n_clusters = n_clusters)
     agg_labels = evaluate_clustering(agg_cluster, "Agglomerative", data, labels_true)
+    print(75 * '-')
+
+    pca_converter = PCA(n_components = 2)
+    data = pca_converter.fit_transform(data)
+
+    plt.figure()
+    plt.title('True labels')
+    plt.scatter(data[:,0], data[:,1], c = labels_true)
+    
+    plt.figure()
+    plt.title('Kmeans labels')
+    plt.scatter(data[:,0], data[:,1], c = kmeans_labels)
+
+    plt.figure()
+    plt.title('Spectral labels')
+    plt.scatter(data[:,0], data[:,1], c = spectral_labels)
+    
+    plt.figure()
+    plt.title('DBSCAN labels')
+    plt.scatter(data[:,0], data[:,1], c = dbscan_labels)
+    
+    plt.figure()
+    plt.title('Agglomerative labels')
+    plt.scatter(data[:,0], data[:,1], c = agg_labels)
+
+    plt.show()
+
+
