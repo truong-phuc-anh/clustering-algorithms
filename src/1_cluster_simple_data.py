99938fb6e6db6848f0f556c41a79de410e9bd03c
Truong Phuc Anh <truongphucanh.main@gmail.com>
2017-10-21 11:55:23 +0700

    doc: Add examples for report

--- src/1_cluster_simple_data.py
+++ src/1_cluster_simple_data.py
@@ -1,36 +1,49 @@
 # Created by Truong Phuc Anh (14520040@gm.uit.edu.vn)
 # Simple implementation of Kmeans cluster
 
+import numpy
 import matplotlib.pyplot    as plt
+from time                   import time
 from sklearn.cluster        import KMeans
-from sklearn.datasets       import make_blobs
+from sklearn.datasets       import make_blobs, make_circles, make_s_curve
 from evaluation             import evaluate_clustering
 
-# 1. Creating data set
-n_samples = 2000
-random_state = 1500
-n_clusters = 2
+# 1. Kmeans is sensitive to initialization
+n_samples = 5000
 n_features = 2
-data, labels_true = make_blobs(n_samples = n_samples, n_features = n_clusters, centers = n_clusters, random_state = random_state)
-
+n_clusters = 3
+data, ground_truth = make_blobs(n_samples = n_samples, n_features = n_features, centers = n_samples % 3, center_box=(-20.0, 20.0), random_state=1500)
 print ('n_samples       : %i' % n_samples)
 print ('n_cluster       : %i' % n_clusters)
 print ('feature space   : %id' % n_features)
+fig = plt.figure()
+ax = fig.add_subplot(2, 5, 1)
+ax.scatter(data[:,0], data[:,1])
+ax.set_title('data')
+for i in xrange (9):
+    kmeans_cluster = KMeans(n_clusters = n_clusters)
+    start_time = time()
+    kmeans_cluster.fit(data)
+    exec_time = time() - start_time
+    print('%-9s\t%.2fs' % (i, exec_time))
+    ax = fig.add_subplot(2, 5, i + 2)
+    ax.scatter(data[:,0], data[:,1], c = kmeans_cluster.labels_)
+    ax.set_title('time: %.2fs' % (exec_time))
 
-# 2. Clustering and evaluating
-print(75 * '-')
-print('cluster\t\ttime\thomo\tcompl\tv-meas\tARI\tAMI')
-kmeans_cluster = KMeans(n_clusters = n_clusters)
-labels_pred = evaluate_clustering(kmeans_cluster, "kmeans", data, labels_true)
-print(75 * '-')
+# 2. Only finds sphericalc clusters
 
-# 3. Visualizing result
-plt.figure()
-plt.title('True labels')
-plt.scatter(data[:,0], data[:,1], c = labels_true)
+data, something = make_circles(n_samples=1000)
+fig = plt.figure()
+ax = fig.add_subplot(1, 2, 1)
+ax.scatter(data[:,0], data[:,1])
+ax.set_title('data')
 
-plt.figure()
-plt.title("kmeans")
-plt.scatter(data[:,0], data[:,1], c = labels_pred)
+kmeans_cluster = KMeans(n_clusters = 2)
+start_time = time()
+kmeans_cluster.fit(data)
+exec_time = time() - start_time
+ax = fig.add_subplot(1, 2, 2)
+ax.scatter(data[:,0], data[:,1], c = kmeans_cluster.labels_)
+ax.set_title('time: %.2fs' % (exec_time))
 
 plt.show()
